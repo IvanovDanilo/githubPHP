5320a9cc32834b80c6b15779adc3dbae52ab80cd
IvanovDanilo <danivanov328@gmail.com>
2024-06-12 14:36:36 +0200

    task1

--- task1.php
+++ task1.php
@@ -1,7 +1,17 @@
+//task 1
 <?php
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 2 == 0) {
         echo $i . " ";
+    }
+}
+//task 2
+
+$numbers = [1, 23, 44, 88, 13, 11, 7, 8];
+
+foreach ($numbers as $number) {
+    if ($number % 2 == 0) {
+        echo $number . " ";
     }
 }
 
